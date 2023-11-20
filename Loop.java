bbff971ff028fa294da0fafa6b15b900c0d26f11
ishwarbokde24 <ishwarbokde786@gmail.com>
2023-11-20 09:31:17 +0530

    added java program

---
+++ Loop.java
@@ -0,0 +1,15 @@
+package day2_loop_array;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		
+		int d;
+		for(d=1;d<=10;d++);
+		System.out.println("hey" + d);
+		
+	
+		
+	}
+
+}
